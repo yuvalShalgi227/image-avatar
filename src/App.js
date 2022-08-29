6101956e1b7a7eceb4dae03ff468a38716c8aa08
Yuval Shalgi <yuvals@perfectomobile.com>
2022-08-29 10:27:06 +0300

    start style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"/>
+      <div className='page-main'>main page</div>
+      <div className='page-main'>main page</div>
+      <div className='page-main'>main page</div>
+    <div className='page-footer'>this is the end</div>
+
     </div>
   );
 }
